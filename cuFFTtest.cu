1b0d09ac3a65262ab996d7944ca7a22bf2a47907
Brian Bauer <ucn2510@n0692.ten.osc.edu>
2016-11-21 21:52:25 -0500

    buffer overflow

--- cuFFTtest.cu
+++ cuFFTtest.cu
@@ -11,7 +11,7 @@ typedef float2 Complex;
 
 void testcuFFT(){
 
-  cufftReal *h_signal = (cufftComplex *)malloc(sizeof(cufftComplex) * SIGNAL_SIZE);
+  cufftReal *h_signal = (cufftReal *)malloc(sizeof(cufftReal) * BATCH_SIZE);
   cufftComplex *h_data = (cufftComplex *)malloc(sizeof(cufftComplex) * (NX/2+1)*BATCH_SIZE);
 
   float ryanSignal [NX] = {0.0,
@@ -23,21 +23,21 @@ void testcuFFT(){
 0.663651234058,
 1.44858683588};
   // Initalize the memory for the signal
-  for (unsigned int i = 0; i < BATCH_SIZE; ++i)
+  for (unsigned int i = 0; i < NX; ++i)
   {
     //h_signal[i].x = rand() / (float)RAND_MAX+1;
-    h_signal[i].x = ryanSignal[i];
+    h_signal[i] = ryanSignal[i];
     //h_signal[i].y = 0;
-    printf("h_signal[%u]: %f\n", i, h_signal[i].x);
+    printf("h_signal[%u]: %f\n", i, h_signal[i]);
   }
 
   cufftHandle plan;
   cufftComplex *d_data;
   cufftReal *d_signal;
-  cudaMalloc((void**)&d_data, sizeof(cufftComplex)*(NX/2+1)*SIGNAL_SIZE);
-  cudaMalloc((void**)&d_signal, sizeof(cufftComplex)*NX);
+  cudaMalloc((void**)&d_data, sizeof(cufftComplex)*(NX/2+1)*BATCH_SIZE);
+  cudaMalloc((void**)&d_signal, sizeof(cufftReal)*NX);
   //cudaMalloc((void**)&d_signal, sizeof(cufftReal)*SIGNAL_SIZE);
-  cudaMemcpy(d_signal, h_signal, sizeof(cufftComplex)*SIGNAL_SIZE, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_signal, h_signal, sizeof(cufftReal)*NX, cudaMemcpyHostToDevice);
 
   free(h_signal);
 
@@ -45,7 +45,7 @@ void testcuFFT(){
     fprintf(stderr, "Cuda error: Failed to allocate\n");
     return; 
   }
-  if(cufftPlan1d(&plan, NX, CUFFT_R2C, SIGNAL_SIZE) != CUFFT_SUCCESS){
+  if(cufftPlan1d(&plan, NX, CUFFT_R2C, BATCH_SIZE) != CUFFT_SUCCESS){
     fprintf(stderr, "CUFFT error: Plan creation failed");
     return; 
   } 
@@ -60,7 +60,7 @@ void testcuFFT(){
     return; 
   }
   
-  cudaMemcpy(h_data,d_data,sizeof(cufftComplex)*SIGNAL_SIZE * (NX/2+1),cudaMemcpyDeviceToHost);
+  cudaMemcpy(h_data,d_data,sizeof(cufftComplex)*BATCH_SIZE * (NX/2+1),cudaMemcpyDeviceToHost);
   for(unsigned int k=0; k<10; k++){
     //printf("h_data[%i]: %f\n",k,h_data[k].x);
     printf("h_data[%u]: %f\n", k, h_data[k].x);
